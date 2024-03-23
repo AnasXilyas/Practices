f499d1fc5d2a2b9dd0d17388e7edd73567e2c04c
AnasXilyas <www.Anasilyas00@gmail.com>
2024-03-23 23:19:30 +0500

    avg salary calculator

---
+++ assingn1.py
@@ -0,0 +1,13 @@
+name1 = input("enter your name: ") 
+print("Name : " , name1)
+salary1 = input("enter your salary: ")
+print("1st person salary is: " , salary1)
+
+salary2 = input("enter your salary: ")
+print("2nd person salary is: " , salary2)
+
+salary3 = input("enter your salary: ")
+print("3rd person salary is: " , salary3)
+
+avgSalary = (int(salary1) + int(salary2) + int(salary3)/3)
+print("Avg of salary is : " , avgSalary)
