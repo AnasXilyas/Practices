a1c816d9cbfabd8b20780f660bf4a9dcdd7f2533
AnasXilyas <www.Anasilyas00@gmail.com>
2024-03-23 11:50:40 +0500

    calculator made

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = input("enter your 1st num: ")
+print("num1=", num1)
+num2 = input("enter your 2nd num: ")
+print("num2=" , num2)
+print("opr + , - , / , *")
+opr = input("enter the opr to perform: ")
+
+if opr == "+":
+    print(int(num1) + int(num2))
+elif opr == "-":
+    print(int(num1) - int(num2))
+elif opr =="*":
+    print(int(num1) * int(num2))
+elif opr == "/":
+    print(int(num1) / int(num2))
+    
+
+
+
