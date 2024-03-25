cfa971002aa36fef877e7218b510e2dccba0c2ab
AnasXilyas <www.Anasilyas00@gmail.com>
2024-03-25 10:35:06 +0500

    pass registor form

---
+++ Registor.py
@@ -0,0 +1,23 @@
+name = input("enter your Name :")
+print("Name =", name)
+Password = input("enter your Password:")
+print("Password =", Password)
+ConfrimPass = input("Confirn your password:")
+
+if name in Password or name[::-1] in Password:
+    print("password cannot be same as name or reverse.")
+
+else:e
+    if len(Password) >= 8 :
+        print("password is strong")
+        if Password == ConfrimPass :
+            print("welcome")
+        else:
+            print("password error")
+    
+    else:
+        print("password is weak")
+
+
+
+
