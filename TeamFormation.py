f1c7dd8208c12f8a25ebee5a79fa9023dbe29eb4
AnasXilyas <www.Anasilyas00@gmail.com>
2024-03-25 23:06:28 +0500

    team formation

---
+++ TeamFormation.py
@@ -0,0 +1,8 @@
+boys = ["abc","abcd","abcde","abcdef"]
+girls = ["aaa","bbb","ccc","ddd","eee"]
+
+if len(boys)==len(girls):
+    print("team is possible")
+else:
+    print("team is not possible")
+    
