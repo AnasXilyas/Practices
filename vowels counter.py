7e5763f5ae5ef87f470ae6e1040046bd022574fb
AnasXilyas <www.Anasilyas00@gmail.com>
2024-03-26 09:37:33 +0500

    vowel counter

---
+++ vowels counter.py
@@ -0,0 +1,14 @@
+userinput = input("Add your sentence : ")
+print("The input is :", userinput)
+
+print("Count of vowel \"a\":" , userinput.count("a"))
+print("Count of vowel \"e\":" , userinput.count("e"))
+print("Count of vowel \"i\":" , userinput.count("i"))
+print("Count of vowel \"o\":" , userinput.count("o")) 
+print("Count of vowel \"u\":" , userinput.count("u"))
+
+
+    
+
+      
+
